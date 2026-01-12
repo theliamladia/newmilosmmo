479c2d9862219a8b8cdc9686d9907a75a0d2f95f
theliamladia <51178138+theliamladia@users.noreply.github.com>
2026-01-12 11:32:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/meetfightersmmo/',
+  base: '/', // Use '/' for local dev, change to '/your-repo-name/' for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
